37877aaf969000f38514549f0ac48e0752fcd1c1
mohit <mohit007sethi@gmail.com>
2025-03-31 16:43:41 +0530

    Order file changes

--- src/main/java/Selenium/Pages/OrderPage.java
+++ src/main/java/Selenium/Pages/OrderPage.java
@@ -44,5 +44,39 @@ public class OrderPage extends BaseClass{
 		return false;
 	}
 	
+	
+//	public boolean verifyOrderHistory(String[] itemNeeded) throws InterruptedException
+//	{	
+//		for(int i=0;i<itemNeeded.length;i++)
+//		{
+//			for(int j=0;j<orderNames.size();j++)
+//			{
+//				String name = orderNames.get(j).getText();
+//				System.out.println(name);
+//				if(name.equalsIgnoreCase(itemNeeded[i]))
+//				{
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+//	public boolean verifyOrderHistory(String[] itemNeeded) throws InterruptedException
+//	{	
+//		for(int i=0;i<itemNeeded.length;i++)
+//		{
+//			for(int j=0;j<orderNames.size();j++)
+//			{
+//				String name = orderNames.get(j).getText();
+//				System.out.println(name);
+//				if(name.equalsIgnoreCase(itemNeeded[i]))
+//				{
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+	
 
 }
